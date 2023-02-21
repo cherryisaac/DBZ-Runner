15ede7b008aa4c6436c437e66adee157e1e797af
cherryisaac <cherryisaac739@yahoo.com>
2023-02-21 16:54:12 -0500

    Animations - done, Music - added, Base game is done!

--- mypygame.py
+++ mypygame.py
@@ -1,6 +1,96 @@
+import random
 import pygame
 from sys import exit
-from random import randint
+from random import randint, choice
+
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        player_walk_1 = pygame.image.load('Graphics/Krillin-remade.png').convert_alpha()
+        player_walk_2 = pygame.image.load('Graphics/Krillin-run.png').convert_alpha()
+        self.player_walk = [player_walk_1, player_walk_2]
+        self.player_index = 0
+        self.player_jump = pygame.image.load('Graphics/Krillin-jump.png').convert_alpha()
+
+        self.image = self.player_walk[self.player_index]
+        self.rect = self.image.get_rect(midbottom = (80, 300))
+        self.gravity = 0
+
+        self.jump_sound = pygame.mixer.Sound('Sound/swoosh-jump.mp3')
+        self.jump_sound.set_volume(0.2)
+
+    def player_input(self):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_SPACE] and self.rect.bottom >= 390:
+            self.gravity = -20
+            self.jump_sound.play()
+
+    def apply_gravity(self):
+        self.gravity += 1
+        self.rect.y += self.gravity
+        if self.rect.bottom >= 390:
+            self.rect.bottom = 390
+
+    def animation_state(self):
+        if self.rect.bottom < 390:
+            self.image = self.player_jump
+        else:
+            self.player_index += 0.1
+            if self.player_index >= len(self.player_walk): self.player_index = 0
+            self.image = self.player_walk[int(self.player_index)]
+                
+
+    def update(self):
+        self.player_input()
+        self.apply_gravity()
+        self.animation_state()
+
+class Obstacle(pygame.sprite.Sprite):
+    def __init__(self, type):
+        super().__init__()
+
+        if type == 'soldier':
+            soldier_frame1 = pygame.image.load('Graphics/soldier.png').convert_alpha()
+            soldier_frame1 = pygame.transform.rotozoom(soldier_frame1, 0, 0.54) 
+            soldier_frame2 = pygame.image.load('Graphics/soldier2.png').convert_alpha()
+            soldier_frame2 = pygame.transform.rotozoom(soldier_frame2, 0, 0.54)
+            self.frames = [soldier_frame1, soldier_frame2]
+            self.animation_increment = 0.01  # soldier animation speed
+            y_pos = 280
+        else:
+            saibaman_frame1 = pygame.image.load('Graphics/saibaman1.png').convert_alpha()
+            saibaman_frame1 = pygame.transform.rotozoom(saibaman_frame1, 0, 0.22) # resize image
+            saibaman_frame2 = pygame.image.load('Graphics/saibaman2.png').convert_alpha()
+            saibaman_frame2 = pygame.transform.rotozoom(saibaman_frame2, 0, 0.28)
+            self.frames = [saibaman_frame1, saibaman_frame2]
+            self.animation_increment = 0.05  # saibaman animation speed
+            y_pos = 385
+
+        self.is_soldier = type == 'soldier'  # attribute to indicate the type of instance
+
+        self.animation_index = 0
+        self.image = self.frames[self.animation_index]
+        self.rect = self.image.get_rect(midbottom = (random.randint(900, 1100), y_pos))
+
+    def animation_state(self):
+        self.animation_index += self.animation_increment  
+        if self.is_soldier and self.animation_index >= len(self.frames):
+            self.animation_index = 0
+        elif not self.is_soldier and self.animation_index >= len(self.frames):
+            self.animation_index = 0
+        self.image = self.frames[int(self.animation_index)]
+
+    def update(self):
+        self.animation_state()
+        self.rect.x -= randint(9, 10) # Speed of objects/enemies
+        self.destroy()
+
+    def destroy(self):
+        if self.rect.x <= -100:
+            self.kill()
+
+
 
 def display_score():
     current_time = int((pygame.time.get_ticks() / 1000)) - start_time
@@ -9,25 +99,19 @@ def display_score():
     screen.blit(score_surf, score_rect)
     return current_time
 
-def obstacle_movement(obstacle_list):
-    if obstacle_list:
-        for obstacle_rect in obstacle_list:
-            obstacle_rect.x -= randint(5, 9)
-
-            if obstacle_rect.bottom == 385: screen.blit(snail_surface, obstacle_rect)
-            else: screen.blit(soldier_surface, obstacle_rect)
-
-        obstacle_list = [obstacle for obstacle in obstacle_list if obstacle.x > -50]
-
-        return obstacle_list
-    else: return []
-
 def collisions(player, obstacles):
     if obstacles:
         for obstacle_rect in obstacles:
             if player.colliderect(obstacle_rect):
                 return False
     return True
+
+def collison_sprites():
+    if pygame.sprite.spritecollide(player.sprite, obstacle_group, False):
+        obstacle_group.empty()
+        return False
+    else:
+        return True
 
 def player_animation():
     global player_surface, player_index
@@ -43,36 +127,26 @@ def player_animation():
 pygame.init()
 
 screen = pygame.display.set_mode((800, 400))
-pygame.display.set_caption('My Game')
+pygame.display.set_caption('DBZ Runner')
 clock = pygame.time.Clock()
 test_font = pygame.font.Font('Fonts/Pixeltype.ttf', 20)
 game_active = False
 start_time = 0
 score = 0
+bg_music = pygame.mixer.Sound('Sound/Dragón Ball Z Chala Head Chala Instrumental (128 kbps).mp3')
+# pygame.mixer.Sound('Sound/We were angels(Angeles Fuimos)-Instrumental (128 kbps).mp3')
+bg_music.set_volume(0.1)
+
+player = pygame.sprite.GroupSingle()
+player.add(Player())
+
+obstacle_group = pygame.sprite.Group()
 
 sky_surface = pygame.image.load('Images/bg.jpeg').convert()
 ground_surface = pygame.image.load('Images/ground-dark.png').convert_alpha()
 
 score_surface = test_font.render('My Game', False, (10, 10, 10))
 score_rect = score_surface.get_rect(center = (400, 50))
-
-# Snail
-snail_frame1 = pygame.image.load('Graphics/saibaman1.png').convert_alpha()
-snail_frame1 = pygame.transform.rotozoom(snail_frame1, 0, 0.22)
-snail_frame2 = pygame.image.load('Graphics/saibaman2.png').convert_alpha()
-snail_frame2 = pygame.transform.rotozoom(snail_frame2, 0, 0.28)
-snail_frames = [snail_frame1, snail_frame2]
-snail_frame_index = 0
-snail_surface = snail_frames[snail_frame_index]
-
-# Soldier
-soldier_frame1 = pygame.image.load('Graphics/soldier.png').convert_alpha()
-soldier_frame1 = pygame.transform.rotozoom(soldier_frame1, 0, 0.54) # resize image
-soldier_frame2 = pygame.image.load('Graphics/soldier2.png').convert_alpha()
-soldier_frame2 = pygame.transform.rotozoom(soldier_frame2, 0, 0.54)
-soldier_frames = [soldier_frame1, soldier_frame2]
-soldier_frame_index = 0
-soldier_surface = soldier_frames[soldier_frame_index]
 
 obstacle_rect_list = []
 
@@ -98,7 +172,7 @@ game_message_rect = game_message.get_rect(center = (400, 350))
 
 # Timer
 obstacle_timer = pygame.USEREVENT + 1
-pygame.time.set_timer(obstacle_timer, 1400) # Frequency of enemies/obstacles
+pygame.time.set_timer(obstacle_timer, randint(640, 1200)) # Frequency of enemies/obstacles
 
 snail_animation_timer = pygame.USEREVENT + 2
 pygame.time.set_timer(snail_animation_timer, 300)
@@ -129,42 +203,30 @@ while True:
 
         if game_active:
             if event.type == obstacle_timer:
-                if randint(0, 2): # 0 = true, 1 = false
-                    obstacle_rect_list.append(snail_surface.get_rect(midbottom = (randint(900, 1100), 385)))
-                else:
-                    obstacle_rect_list.append(soldier_surface.get_rect(midbottom = (randint(900, 1100), 280)))
-                
-            if event.type == snail_animation_timer:
-                if snail_frame_index == 0: snail_frame_index = 1
-                else: snail_frame_index = 0
-                snail_surface = snail_frames[snail_frame_index]
-
-            if event.type == soldier_animation_timer:
-                if soldier_frame_index == 0: soldier_frame_index = 1
-                else: soldier_frame_index = 0
-                soldier_surface = soldier_frames[soldier_frame_index]
+                # Frequency of enemy types - currently set at 40/60
+                obstacle_group.add(Obstacle(choice(['soldier', 'soldier', 'saibaman', 'saibaman', 'saibaman'])))
+            
        
     if game_active:
+        bg_music.play(loops = -1)
         # screen block image transfer aka one surface on another surface & positioning
         screen.blit(sky_surface,(-300,0)) 
         screen.blit(ground_surface,(0,360))
         score = display_score()
 
-        # Player
-        player_gravity +=1
-        player_rect.y += player_gravity
-        if player_rect.bottom >= 390:
-            player_rect.bottom = 390
-        player_animation()
-        screen.blit(player_surface, player_rect)
-
-        # Obstacle movement 
-        obstacle_rect_list = obstacle_movement(obstacle_rect_list)
+        # player
+        player.draw(screen)
+        player.update()
+ 
+        # obstacles
+        obstacle_group.draw(screen)
+        obstacle_group.update()
 
         #collisions
-        game_active = collisions(player_rect, obstacle_rect_list)
+        game_active = collison_sprites()
 
     else:
+        bg_music.stop( )
         screen.fill('black')
         screen.blit(player_alt, player_alt_rect)
         obstacle_rect_list.clear()
